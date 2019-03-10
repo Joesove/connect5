4927d09ac7a326ceaf13e9b540b0c4013ad84925
Joseph Harte <joseph.harte.jh@gmail.com>
2019-03-10 14:48:35 +0000

    Updated client

--- client.py
+++ client.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
 
+# Created by : Joseph Harte
+#       Date : March 2019
+#    Version : 1.0
+
 from socket import AF_INET, socket, SOCK_STREAM, gethostname
 import json
 import logging
@@ -32,7 +36,13 @@ def main():
 	
 	# Start the main client loop
 	while True:
-		msg = json.loads( client.recv(buf_size).decode("utf8") )
+		try:
+			msg = json.loads( client.recv(buf_size).decode("utf8") )
+		except:
+			print ("Disconnected from the server.")
+			logging.error("Disconnected from the server")
+			sys.exit(1)
+
 		if msg["cmd"] == "INFO":
 			# Get the player name form the user
 			name = input("Please input your name and press enter: ")
@@ -41,18 +51,39 @@ def main():
 			response = {"done": True, "data": name}
 			send_msg(client, response)
 
-		elif msg["cmd"] == "WAIT":
-			pass
-		elif msg["cmd"] == "PRINT":
-			print (msg["data"])
-		elif msg["cmd"] == "TURN":
-			turn = input("It’s your turn %s, please enter column (1-9): " % msg["data"])
-			logging.info("Name: %s" % name)
+		elif msg["cmd"] == "QUIT":
+			# Quit the game
+			print ("Quitting game.")
+			logging.info("Quitting game.")
 
-			response = {"done": True, "data": turn}
+			response = {"done": True, "data": ""}
+			send_msg(client, response)
+
+			client.close()
+			sys.exit(0)
+		elif msg["cmd"] == "PRINT":
+			# Print a message from the server
+			print (msg["data"])
+
+			response = {"done": True, "data": ""}
+			send_msg(client, response)
+		elif msg["cmd"] == "TURN":
+			# Prompt the player for their turn
+			while True:
+				turn = input("It’s your turn %s, please enter column (1-9): " % msg["data"])
+				try:
+					int(turn)
+					break
+				except:
+					print("You did not enter a number, try again.")
+
+			response = {"done": True, "data": int(turn) - 1}
 			send_msg(client, response)
 		else:
 			logging.error("Unkown command")
 
 if __name__ == "__main__":
-	main()
+	try:
+		main()
+	except KeyboardInterrupt:
+		sys.exit(0)
